a05a7c9074694780e2540575cf691b9fb57a0a45
Alejandro Matos <amatos@gmail.com>
2021-11-14 11:23:19 -0500

    removing zsh

--- 2-user.sh
+++ 2-user.sh
@@ -54,10 +54,10 @@ $installpkg
 
 export PATH=$PATH:~/.local/bin
 cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
-pip install konsave
-konsave -i $HOME/ArchTitus/kde.knsv
+# pip install konsave
+# konsave -i $HOME/ArchTitus/kde.knsv
 sleep 1
-konsave -a kde
+# konsave -a kde
 
 echo -e "\nDone!\n"
 exit
